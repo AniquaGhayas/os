702fe6759dabce12272b7211416c2bbcfbaf63c8
AniquaGhayas <aniquaghayas@gmail.com>
2025-11-20 00:15:52 +0530

    Banker'sAlgorithm-DeadLockAvoidance.c

---
+++ Banker'sAlgorithm-DeadLockAvoidance.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    printf("Enter number of resources: ");
+    scanf("%d", &m);
+
+    int alloc[n][m], max[n][m], avail[m], need[n][m];
+
+    printf("Enter Allocation Matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            scanf("%d", &alloc[i][j]);
+
+    printf("Enter Max Matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            scanf("%d", &max[i][j]);
+
+    printf("Enter Available Resources:\n");
+    for (int j = 0; j < m; j++)
+        scanf("%d", &avail[j]);
+
+    // Calculate Need = Max - Allocation
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            need[i][j] = max[i][j] - alloc[i][j];
+
+    int finish[n], safe[n], count = 0;
+    for (int i = 0; i < n; i++) finish[i] = 0;
+
+    while (count < n) {
+        int found = 0;
+        for (int i = 0; i < n; i++) {
+            if (!finish[i]) {
+                int j;
+                for (j = 0; j < m; j++)
+                    if (need[i][j] > avail[j]) break;
+
+                if (j == m) {
+                    for (int k = 0; k < m; k++)
+                        avail[k] += alloc[i][k];
+                    safe[count++] = i;
+                    finish[i] = 1;
+                    found = 1;
+                }
+            }
+        }
+        if (!found) break;
+    }
+
+    if (count == n) {
+        printf("\nSystem is in SAFE STATE.\nSafe sequence: ");
+        for (int i = 0; i < n; i++)
+            printf("P%d ", safe[i]);
+    } else {
+        printf("\nSystem is in DEADLOCK (Unsafe State)\n");
+    }
+
+    return 0;
+}
